9e15ed5a2047dc2717f40005ed9078c826eb5a7b
Alex Silva <alexs1010@gmail.com>
2022-03-10 16:39:17 -0600

    crud for companies
    Co-authored-by: alanRazoMen <arazo@ucol.mx>
    Co-authored-by: AlejandroCuevasZepeda <acuevas@ucol.mx>
    Co-authored-by: MrCano369x <canorueda2000@gmail.com>

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,10 +26,8 @@ class EmployeesController < ApplicationController
     respond_to do |format|
       if @employee.save
         format.html { redirect_to employee_url(@employee), notice: "Employee was successfully created." }
-        format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,10 +37,8 @@ class EmployeesController < ApplicationController
     respond_to do |format|
       if @employee.update(employee_params)
         format.html { redirect_to employee_url(@employee), notice: "Employee was successfully updated." }
-        format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +49,6 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to employees_url, notice: "Employee was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
--- app/views/employees/_employee.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! employee, :id, :name, :email, :position, :employee_number, :private_number, :created_at, :updated_at
-json.url employee_url(employee, format: :json)
