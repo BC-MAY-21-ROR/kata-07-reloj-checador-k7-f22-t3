fdaf10014cc4e13ce6f2e7e388ab08b042110582
MrCano369x <canorueda2000@gmail.com>
2022-03-17 17:11:51 -0600

    corrections

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :company_branches
   resources :employees
-  
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
