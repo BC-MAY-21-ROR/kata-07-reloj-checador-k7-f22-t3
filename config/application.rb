4bf94fc21065f8b1f545ac0561e50aa4e3dce014
Alex Silva <alexs1010@gmail.com>
2022-03-25 15:22:50 -0600

    Change test framework and setting up

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,14 @@ module Kata07RelojChecadorK7F22T3
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures:         false,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        request_specs:    false,
+        controller_specs: false
+    end
   end
 end
