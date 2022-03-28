862a58aa9c501cfbe8e900f1182eb4f9b9cf4274
Alex Silva <alexs1010@gmail.com>
2022-03-28 14:14:00 -0600

    add basic testing for employee model and fix with model validations
    Co-authored-by: alanRazoMen <alanRazoMen@users.noreply.github.com>
    Co-authored-by: AlejandroCuevasZepeda <AlejandroCuevasZepeda@users.noreply.github.com>
    Co-authored-by: MrCano369x <canorueda2000@gmail.com>

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,23 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                :bigint           not null, primary key
+#  name              :string
+#  email             :string
+#  position          :string
+#  employee_number   :integer
+#  private_number    :string
+#  company_branch_id :bigint
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 class Employee < ApplicationRecord
   belongs_to :company_branch
   has_many :attendances
+  
+  validates :name, :email, :position, :employee_number, :private_number, :company_branch_id, presence: true
+  validates :email, uniqueness: true
+  validates :private_number, uniqueness: true
+  validates :employee_number, uniqueness: true
 end
