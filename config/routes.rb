2c766fdad36d7f31594e11cfc6603a7980c1d774
Alex Silva <alexs1010@gmail.com>
2022-03-30 06:08:59 -0600

    Create report of absence and refactor reports controller, add field to employee to check if is active, and general improves and refactors
    Co-authored-by: alanRazoMen <alanRazoMen@users.noreply.github.com>
    Co-authored-by: AlejandroCuevasZepeda <AlejandroCuevasZepeda@users.noreply.github.com>
    Co-authored-by: MrCano369x <canorueda2000@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'reports/show'
   resources :attendances
   resources :company_branches
   resources :employees
   devise_for :users
   resources :users
+  resources :reports, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
