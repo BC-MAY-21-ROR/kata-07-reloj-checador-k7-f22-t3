486b2f538ad1fb3a796325ce3d0510a7ceea7783
Alex Silva <alexs1010@gmail.com>
2022-03-30 06:08:59 -0600

    Create report of absence and refactor reports controller, add field to employee to check if is active, and general improves and refactors
    Co-authored-by: alanRazoMen <alanRazoMen@users.noreply.github.com>
    Co-authored-by: AlejandroCuevasZepeda <AlejandroCuevasZepeda@users.noreply.github.com>
    Co-authored-by: MrCano369x <canorueda2000@gmail.com>

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,29 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                :bigint           not null, primary key
+#  name              :string
+#  email             :string
+#  position          :string
+#  employee_number   :integer
+#  private_number    :string
+#  company_branch_id :bigint
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  is_active         :boolean
+#
 class Employee < ApplicationRecord
   belongs_to :company_branch
   has_many :attendances
+
+  scope :actives, -> { where(is_active: true) }
+
+  def absence_by_month
+    attendances.where.not(check_in: nil).where('check_in BETWEEN ? AND ?', DateTime.now.beginning_of_month, DateTime.now.end_of_month).count
+  end
+
+  def today_attendance?
+    attendances.where(check_in: Date.today.all_day).first.nil? ? false : true
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_235621) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_231523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_235621) do
     t.bigint "company_branch_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_active"
     t.index ["company_branch_id"], name: "index_employees_on_company_branch_id"
   end
 
